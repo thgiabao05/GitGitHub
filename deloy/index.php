e5d58ad70eca4becc5c6b3e9a16f2d410046e787
05 Gia Bảo <gudboy2k4nek@gmail.com>
2025-12-19 15:42:47 +0700

    sua lan 1

---
+++ deloy/index.php
@@ -0,0 +1,6 @@
+<?php
+  echo "<h1>Trương Hoàng Gia Bảo - Ca cuối</h1>";
+  echo "<p>Trạng thái: Chạy thành công trên Vercel!</p>";
+  echo "<p>Thời gian hiện tại: " . date('Y-m-d H:i:s') . "</p>";
+  // phpinfo(); // Bạn có thể mở dòng này để xem cấu hình PHP của Vercel
+?>

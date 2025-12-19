3b8dfc1511a9391ce97ba8c44f7e2a4d9e3f9e09
05 Gia Bảo <gudboy2k4nek@gmail.com>
2025-12-19 15:52:11 +0700

    sua lan 12

--- Project1_PHP/index.php
+++ Project1_PHP/index.php
@@ -1,4 +1,4 @@
 <?php
   echo "<h1>Trương Hoàng Gia Bảo - Ca Cuối</h1>";
-  // phpinfo(); // Bạn có thể mở dòng này để xem cấu hình PHP của Vercel
+  phpinfo(); // Bạn có thể mở dòng này để xem cấu hình PHP của Vercel
 ?>
